61fc6a9af5b24b299c6e98363f8575e1c50b990a
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2022-02-10 00:16:43 +0900

    講座用のソースコードの追加

---
+++ js/index7-1.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', function () {
+	const item = document.getElementById("sec1")
+	item.style.display = "block";
+	const item2 = document.getElementById("sec2")
+	item2.style.display = "none";
+	const item3 = document.getElementById("sec3")
+	item3.style.display = "none";
+})
+
+function func1() {
+	const item = document.getElementById("sec1")
+	item.style.display = "block";
+	const item2 = document.getElementById("sec2")
+	item2.style.display = "none";
+	const item3 = document.getElementById("sec3")
+	item3.style.display = "none";
+}
+function func2() {
+	const item = document.getElementById("sec1")
+	item.style.display = "none";
+	const item2 = document.getElementById("sec2")
+	item2.style.display = "block";
+	const item3 = document.getElementById("sec3")
+	item3.style.display = "none";
+
+}
+function func3() {
+	const item = document.getElementById("sec1")
+	item.style.display = "none";
+	const item2 = document.getElementById("sec2")
+	item2.style.display = "none";
+	const item3 = document.getElementById("sec3")
+	item3.style.display = "block";
+
+}
