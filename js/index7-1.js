9bb2e1316c99874302b9e2a80a26184f422a4112
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2022-02-12 01:03:42 +0900

    原稿書き上げ終了

--- js/index7-1.js
+++ js/index7-1.js
@@ -1,11 +1,13 @@
-window.addEventListener('DOMContentLoaded', function () {
-	const item = document.getElementById("sec1")
-	item.style.display = "block";
-	const item2 = document.getElementById("sec2")
-	item2.style.display = "none";
-	const item3 = document.getElementById("sec3")
-	item3.style.display = "none";
-})
+// JavaScript code
+
+// window.addEventListener('DOMContentLoaded', function () {
+// 	const item = document.getElementById("sec1")
+// 	item.style.display = "block";
+// 	const item2 = document.getElementById("sec2")
+// 	item2.style.display = "none";
+// 	const item3 = document.getElementById("sec3")
+// 	item3.style.display = "none";
+// })
 
 function func1() {
 	const item = document.getElementById("sec1")
